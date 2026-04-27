114f08b905d2820129b276590e686c4d15c319f3
Munk1990 <mayank.kr@pm.me>
2026-04-27 17:17:54 -0400

    Bugfix: Context attachment

--- src/AidaWidget/hooks/useAttachments.js
+++ src/AidaWidget/hooks/useAttachments.js
@@ -276,6 +276,7 @@ export const useAttachments = (setSelectedModel) => {
         addUrlAttachment,
         removeAttachment,
         clearAttachments,
+        addContextAttachment,
         isAttachmentModalOpen,
         openModal,
         closeModal
